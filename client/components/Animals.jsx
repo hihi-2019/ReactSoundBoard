684ceeaeee78cb1fefe2ec6e54da139cd5cdee19
aliceals <alice.alsford@gmail.com>
2019-11-08 11:39:03 +1300

    Animals working

--- client/components/Animals.jsx
+++ client/components/Animals.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Sky from "react-sky";
 
+// Images
 import cow from "../../public/images/001-cow.png";
 import dog from "../../public/images/002-dog.png";
 import cat from "../../public/images/003-cat.png";
@@ -8,27 +9,54 @@ import sheep from "../../public/images/004-sheep.png";
 import duck from "../../public/images/005-duck.png";
 
 //Sounds
-// import cowSound from "../../public/sounds/cow.wav"
+import cowSound from "../../public/sounds/cow.wav";
+import dogSound from "../../public/sounds/dog.mp3";
+import catSound from "../../public/sounds/cat.wav";
+import duckSound from "../../public/sounds/duck.wav";
+import sheepSound from "../../public/sounds/sheep.wav";
 
+// Cow
+const cowFile = "8P8BfWjJzJK9jsYAAAAASUVORK5CYII=";
+const cowAudio = new Audio(cowSound);
 
+// Dog
+const dogFile = "HyidtB1VHyhsAAAAAElFTkSuQmCC";
+const dogAudio = new Audio(dogSound);
 
-// Images
-// const cow = "https://image.flaticon.com/icons/svg/2174/2174039.svg"
+//Cat
+const catFile = "j02bwAAAABJRU5ErkJggg==";
+const catAudio = new Audio(catSound);
 
+//Duck
+const duckFile = "grCSa1AAAAAElFTkSuQmCC";
+const duckAudio = new Audio(duckSound);
 
-// Sounds
-// const cowAudio = new Audio(cowSound)
+//Sheep
+const sheepFile = "wEoQcKbyfVxwwAAAABJRU5ErkJggg==";
+const sheepAudio = new Audio(sheepSound);
 
 class Animals extends Component {
-    handleClick = (event) =>{
-        console.log(event.target)
+    handleClick = event => {
+        console.log(event.target);
 
-        // if (event.target.srcset == cow){
-        //   cowAudio.play()
-        // }
-    }
+        if (event.target.srcset.includes(cowFile)) {
+            console.log("cow");
+            cowAudio.play();
+        } else if (event.target.srcset.includes(dogFile)) {
+            console.log("dog");
+            dogAudio.play();
+        } else if (event.target.srcset.includes(catFile)) {
+            console.log("CAT");
+            catAudio.play();
+        } else if (event.target.srcset.includes(duckFile)) {
+            console.log("duck");
+            duckAudio.play();
+        } else if (event.target.srcset.includes(sheepFile)) {
+            console.log("sheep");
+            sheepAudio.play();
+        }
+    };
 
-    
     render() {
         return (
             <div onClick={this.handleClick}>
